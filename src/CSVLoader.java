584bc5922eaa88e50f7fd582c81c0613a064ff6a
willfeighner <read4ever2@gmail.com>
2021-11-14 18:11:15 -0600

    working loader

--- src/CSVLoader.java
+++ src/CSVLoader.java
@@ -1,18 +1,46 @@
 /*
-*  Filename: Main.java
-*  Author: Will Feighner
-*  Date: 2021 11 14
-* Purpose: Implements the banker's algorithm from a properly
-* formatted text file of resources and processes.
-*
-* */
+ *  Filename: Main.java
+ *  Author: Will Feighner
+ *  Date: 2021 11 14
+ * Purpose: Implements the banker's algorithm from a properly
+ * formatted text file of resources and processes.
+ *
+ * */
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class CSVLoader {
 
   public final String filepath;
 
   CSVLoader(String filepath) {
-
     this.filepath = filepath;
+  }
+
+  public int[][] getValues() {
+    int[][] matrix = new int[0][1];
+    String line;
+    String[] tokens;
+    try {
+      BufferedReader bufferedReader = new BufferedReader(new FileReader(filepath));
+
+      int resources = Integer.parseInt(bufferedReader.readLine().split(",")[1]);
+      int processes = Integer.parseInt(bufferedReader.readLine().split(",")[1]);
+
+      int j = 0;
+      matrix = new int[(processes * 2) + 1][resources];
+      while ((line = bufferedReader.readLine()) != null) {
+        tokens = line.split(",");
+        for (int i = 0; i < tokens.length - 1; i++) {
+          matrix[j][i] = Integer.parseInt(tokens[i + 1]);
+        }
+        j++;
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return matrix;
   }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,19 @@
 */
 
 
-
 public class Main {
   public static void main(String[] args) {
+    CSVLoader loader = new CSVLoader("/Users/willfeighner/IdeaProjects/FeighnerCMSC412HW4/src/TestFile.csv");
+    int[][] bankersArray = loader.getValues();
+    int[] resources = bankersArray[0];
+    int[][] allocated = new int[bankersArray.length][(bankersArray[0].length - 1) / 2];
+    int[][] maxRequest = new int[bankersArray.length][(bankersArray[0].length - 1) / 2];
 
+    for (int[] ints : bankersArray) {
+      for (int j = 0; j < bankersArray[0].length; j++) {
+        System.out.print(ints[j] + ", ");
+      }
+      System.out.println();
+    }
   }
 }
